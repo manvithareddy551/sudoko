745f938351f3a631e4805af768365796cf4b8bfb
manvithareddy551 <84329514+manvithareddy551@users.noreply.github.com>
2022-08-02 18:32:55 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,113 @@
+
+
+
+var board = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
+
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
+ function sol(){
+ document.getElementById("1").textContent= 3;
+ document.getElementById("2").textContent= 8;
+ document.getElementById("3").textContent= 7;
+ document.getElementById("4").textContent= 4;
+ document.getElementById("5").textContent= 9;
+ document.getElementById("6").textContent= 1;
+ document.getElementById("7").textContent= 6;
+ document.getElementById("8").textContent= 2;
+ document.getElementById("9").textContent= 5;
+ document.getElementById("10").textContent= 2;
+ document.getElementById("11").textContent= 4;
+ document.getElementById("12").textContent= 1;
+ document.getElementById("13").textContent= 5;
+ document.getElementById("14").textContent= 6;
+ document.getElementById("15").textContent= 8;
+ document.getElementById("16").textContent= 3;
+ document.getElementById("17").textContent= 7;
+ document.getElementById("18").textContent= 9;
+ document.getElementById("19").textContent= 5;
+ document.getElementById("20").textContent= 6;
+ document.getElementById("19").textContent= 9;
+ document.getElementById("20").textContent= 3;
+ document.getElementById("21").textContent= 2;
+ document.getElementById("22").textContent= 4;
+ document.getElementById("23").textContent= 7;
+ document.getElementById("24").textContent= 0;
+ document.getElementById("25").textContent= 1;
+ document.getElementById("26").textContent= 3;
+ document.getElementById("27").textContent= 4;
+ document.getElementById("28").textContent= 5;
+ document.getElementById("29").textContent= 8;
+ document.getElementById("30").textContent= 6;
+ document.getElementById("31").textContent= 1;
+ document.getElementById("32").textContent= 9;
+ document.getElementById("33").textContent= 2;
+ document.getElementById("34").textContent= 3;
+ document.getElementById("35").textContent= 4;
+ document.getElementById("36").textContent= 1;
+ document.getElementById("37").textContent= 2;
+ document.getElementById("38").textContent= 3;
+ document.getElementById("39").textContent= 7;
+ document.getElementById("40").textContent= 8;
+ document.getElementById("41").textContent= 4;
+ document.getElementById("42").textContent= 5;
+ document.getElementById("43").textContent= 9;
+ document.getElementById("44").textContent= 6;
+ document.getElementById("45").textContent= 4;
+ document.getElementById("46").textContent= 9;
+ document.getElementById("47").textContent= 6;
+ document.getElementById("48").textContent= 5;
+ document.getElementById("49").textContent= 3;
+ document.getElementById("50").textContent= 1;
+ document.getElementById("51").textContent= 8;
+ document.getElementById("52").textContent= 7;
+ document.getElementById("53").textContent= 9;
+ document.getElementById("54").textContent= 3;
+ document.getElementById("55").textContent= 4;
+ document.getElementById("56").textContent= 1;
+ document.getElementById("57").textContent= 7;
+ document.getElementById("58").textContent= 6;
+ document.getElementById("59").textContent= 8;
+ document.getElementById("60").textContent= 5;
+ document.getElementById("61").textContent= 2;
+ document.getElementById("62").textContent= 6;
+ document.getElementById("63").textContent= 7;
+ document.getElementById("64").textContent= 5;
+ document.getElementById("65").textContent= 8;
+ document.getElementById("66").textContent= 3;
+ document.getElementById("67").textContent= 2;
+ document.getElementById("68").textContent= 9;
+ document.getElementById("69").textContent= 4;
+ document.getElementById("70").textContent= 1;
+ document.getElementById("71").textContent= 8;
+ document.getElementById("72").textContent= 1;
+ document.getElementById("73").textContent= 2;
+ document.getElementById("74").textContent= 9;
+ document.getElementById("75").textContent= 4;
+ document.getElementById("76").textContent= 9;
+ document.getElementById("77").textContent= 4;
+ document.getElementById("78").textContent= 5;
+ document.getElementById("79").textContent= 7;
+ document.getElementById("80").textContent= 6;
+ document.getElementById("81").textContent= 3;
+ 
+ }
+    
